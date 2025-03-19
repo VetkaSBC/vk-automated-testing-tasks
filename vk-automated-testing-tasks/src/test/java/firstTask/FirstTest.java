1322d0fb7444998c60d43ebf3ba1518c7f18f4ab
VetkaSBC <dedgove@gmail.com>
2025-03-19 20:40:04 +0300

    First test

---
+++ vk-automated-testing-tasks/src/test/java/firstTask/FirstTest.java
@@ -0,0 +1,14 @@
+package firstTask;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstTest {
+
+    @Test
+    public void main(){
+        assertTrue((28+54)/2+(6*6)==77);
+    }
+}
